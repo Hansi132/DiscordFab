a67318b5ca13d328c5ca92a0ce65143229af8bce
Hans Kristian <hanskristianom@gmail.com>
2021-04-24 18:09:24 +0200

    Added bantime to the embed.

--- src/main/java/com/github/hansi132/discordfab/discordbot/listener/BanSender.java
+++ src/main/java/com/github/hansi132/discordfab/discordbot/listener/BanSender.java
@@ -7,13 +7,14 @@ import org.kilocraft.essentials.api.event.player.PlayerBannedEvent;
 import java.awt.*;
 import java.util.Date;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class BanSender {
     public static void sendBan(PlayerBannedEvent playerBannedEvent) {
         String victimName = playerBannedEvent.getVictim().getName();
         String staffName = playerBannedEvent.getSource().getName();
         String reason = playerBannedEvent.getReason();
-        long expiry = playerBannedEvent.getExpiry();
+        Date expiry = new Date(playerBannedEvent.getExpiry());
         boolean ipBan = playerBannedEvent.isIpBan();
         boolean permanentBan = playerBannedEvent.isPermanent();
         boolean silent = playerBannedEvent.isSilent();
@@ -23,6 +24,16 @@ public class BanSender {
                 (ipBan ? " ipBan" : "") +
                 (silent ? " performed silent" : "");
 
+        Date from = new Date();
+        int banTimeInMillis = (int) Math.abs(expiry.getTime() - from.getTime());
+        long diff = TimeUnit.HOURS.convert(banTimeInMillis, TimeUnit.MILLISECONDS) + 1;
+        String banTimeText = diff + " hours";
+
+        if (diff >= 24) {
+            diff = diff / 24;
+            banTimeText = diff + " days";
+        }
+
         EmbedBuilder embedBuilder = new EmbedBuilder()
             .setColor(color)
             .setTitle(title)
@@ -31,8 +42,10 @@ public class BanSender {
             if (permanentBan) {
                 embedBuilder.addField("Time expires:", "Never", false);
             } else {
-                embedBuilder.addField("Time expires:", new Date(expiry).toString(), false);
+                embedBuilder.addField("Time expires:", expiry.toString(), false);
+                embedBuilder.addField("Ban time:", banTimeText, false);
             }
+
 
         Objects.requireNonNull(DiscordFab.getBot().getTextChannelById(DiscordFab.getInstance().getConfig().banSenderChat)).sendMessage(embedBuilder.build()).queue();
 
